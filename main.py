04f334c06547aa06957a270617444f165ec3c661
Georgios Tsirigos <geo.tsirigos@gmail.com>
2024-09-04 10:03:28 +0300

    Update main.py

--- main.py
+++ main.py
@@ -117,12 +117,11 @@ def compare_texts(ocr_text: str, original_text: str) -> str:
     for char in diff:
         if char.startswith(' '):  # no difference
             highlighted_text.append(f'<span style="color:green">{char[2:]}</span>')
-        elif char.startswith('-'):  # missing in OCR (in original but not in OCR)
+        elif char.startswith('-'):  # missing in OCR
             highlighted_text.append(f'<span style="color:red">{char[2:]}</span>')
-        elif char.startswith('+'):  # extra in OCR (in OCR but not in original)
+        elif char.startswith('+'):  # extra in OCR
             highlighted_text.append(f'<span style="color:blue">{char[2:]}</span>')
 
-    # Join the list into a string and return the highlighted HTML
     return ''.join(highlighted_text)
 
 
