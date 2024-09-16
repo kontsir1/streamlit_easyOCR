d5ac8cc024419b7e7eabf1d9f5313dbee6ffb1eb
Georgios Tsirigos <geo.tsirigos@gmail.com>
2024-09-16 13:48:35 +0300

    Create constants.py

---
+++ constants.py
@@ -0,0 +1,15 @@
+# constants.py
+AVAILABLE_LANGUAGES = {
+    "English": "en",
+    "French": "fr",
+    "Italian": "it",
+    "Polish": "pl",
+    "German": "de",
+    "Spanish": "es",
+    "Chinese (Simplified)": "ch_sim",
+    "Chinese (Traditional)": "ch_tra",
+    "Japanese": "ja",
+    "Korean": "ko",
+    "Russian": "ru",
+    "Arabic": "ar",
+}
